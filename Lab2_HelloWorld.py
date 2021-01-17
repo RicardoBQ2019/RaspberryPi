9e4e4ab3cca22caa9937be59fd397b79480ce069
RicardoBQ2019 <ricardo.bquintero@gmail.com>
2021-01-16 21:45:27 -0400

    first commit

---
+++ Lab2_HelloWorld.py
@@ -0,0 +1,31 @@
+""" /**************************************************
+This is the first part of the training course of Python
+Author: Ricardo Bautista Quintero
+Jan 06 2021
+NSCC 
+Ivany Campus
+/**************************************************"""
+
+"""
+We'll learn about the following topics:
+
+1.) Types of Numbers in Python
+2.) Basic Arithmetic
+3.) Differences between classic division and floor division
+4.) Object Assignment in Python
+    
+"""
+def program_1():
+    a = 2
+    f = 2e4
+    print("Hello World")
+    print(a)
+    print("is an integer")
+    print(f)
+    print("is an floating point number")
+    
+ 
+if __name__ == '__main__':
+    program_1()
+
+
